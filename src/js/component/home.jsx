217cf8b5d274e6de1e02a9ec9d6dfffd120a3e3f
James Grover <jgrovermail@gmail.com>
2019-04-05 17:13:58 +0000

    initial commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import TrafficLight from "./TrafficLight.jsx";
+
+//create your first component
+class Home extends React.Component {
+	render() {
+		return (
+			<div>
+				<TrafficLight />
+			</div>
+		);
+	}
+}
+export default Home;
